6c134ec7c687b5985cdb779943cdab096a717d7b
Hvemvilhvad <malthewbirch@gmail.com>
2025-05-20 12:21:26 +0200

    is done.

--- Eksamensprojekt/Necrotroph_Eksamensprojekt/Necrotroph_Eksamensprojekt/TimeLineManager.cs
+++ Eksamensprojekt/Necrotroph_Eksamensprojekt/Necrotroph_Eksamensprojekt/TimeLineManager.cs
@@ -23,21 +23,35 @@ namespace Necrotroph_Eksamensprojekt
         {
             TimeLineEvents = new Dictionary<int, (float time, Action action)>();
             nextID = 0;
-
         }
         #endregion
         #region Methods
         /// <summary>
         /// Adds an event to the timeline, that will be handled after a set amount of time.
         /// </summary>
-        /// <param name="time">The time untill the event is executed/handled.</param>
+        /// <param name="time">The time untill the event is executed/handled, in milisecounds.</param>
         /// <param name="action">The action that is ran once the time has passed.</param>
+        /// <returns>The events ID, which is used for removal.</returns>
         public static int AddEvent(float time, Action action)
         {
             TimeLineEvents.Add(nextID, (time, action));
             return nextID++;
         }
 
+        /// <summary>
+        /// Removes the event with the given ID.
+        /// </summary>
+        /// <param name="ID">The given ID.</param>
+        /// <returns>true, if successfully removed.</returns>
+        public static bool RemoveEvent(int ID)
+        {
+            return TimeLineEvents.Remove(ID);
+        }
+
+        /// <summary>
+        /// Updates and event time and handles the events.
+        /// </summary>
+        /// <param name="gameTime">Gametime object.</param>
         public static void Update(GameTime gameTime)
         {
             Dictionary<int, (float time, Action action)> tempTimeline = new Dictionary<int, (float time, Action action)>();
@@ -45,7 +59,14 @@ namespace Necrotroph_Eksamensprojekt
             {
                 (float time, Action action) @event = timeLineEvent.Value;
                 @event.time -= gameTime.ElapsedGameTime.Milliseconds;
-                tempTimeline.Add(timeLineEvent.Key, @event);
+                if (@event.time <= 0)
+                {
+                    @event.action();
+                }
+                else
+                {
+                    tempTimeline.Add(timeLineEvent.Key, @event);
+                }
             }
             TimeLineEvents = tempTimeline;
         }
