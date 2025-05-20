3242341ce1b5295bd2565565c4a3bfd137670c7c
Hargaflarb <emmabrocher@gmail.com>
2025-05-20 13:05:38 +0200

    set up object pool classes (they don't do anything yet)

---
+++ Eksamensprojekt/Necrotroph_Eksamensprojekt/Necrotroph_Eksamensprojekt/ObjectPools/ObjectPool.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Necrotroph_Eksamensprojekt.ObjectPools
+{
+    public abstract class ObjectPool
+    {
+        #region Fields
+        private List<GameObject> active = new List<GameObject>();
+        private List<GameObject> inactive = new List<GameObject>();
+        #endregion
+        #region Properties
+        #endregion
+        #region Constructors
+        #endregion
+        #region Methods
+        public GameObject GetObject()
+        {
+            return null;
+        }
+        public void ReleaseObject(GameObject obj)
+        {
+
+        }
+
+        protected abstract GameObject Create();
+
+        protected abstract void CleanUp(GameObject obj);
+        #endregion
+    }
+}
---
+++ Eksamensprojekt/Necrotroph_Eksamensprojekt/Necrotroph_Eksamensprojekt/ObjectPools/TreePool.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Necrotroph_Eksamensprojekt.ObjectPools
+{
+    public class TreePool : ObjectPool
+    {
+        #region Fields
+        private static TreePool instance;
+        #endregion
+        #region Properties
+        public static TreePool Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new TreePool();
+                }
+                return instance;
+            }
+        }
+        #endregion
+        #region Constructors
+        private TreePool()
+        {
+
+        }
+        #endregion
+        #region Methods
+        protected override GameObject Create()
+        {
+            throw new NotImplementedException();
+        }
+        protected override void CleanUp(GameObject obj)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
