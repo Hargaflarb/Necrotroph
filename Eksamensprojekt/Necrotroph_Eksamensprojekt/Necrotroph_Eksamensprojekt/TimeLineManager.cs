832dcbaa79e22ed02a9516507424ace579312b2b
Hvemvilhvad <malthewbirch@gmail.com>
2025-05-20 10:57:18 +0200

    somn happend, timeline halfway-esque

--- Eksamensprojekt/Necrotroph_Eksamensprojekt/Necrotroph_Eksamensprojekt/TimeLineManager.cs
+++ Eksamensprojekt/Necrotroph_Eksamensprojekt/Necrotroph_Eksamensprojekt/TimeLineManager.cs
@@ -9,15 +9,46 @@ using Microsoft.Xna.Framework.Input;
 
 namespace Necrotroph_Eksamensprojekt
 {
-    public class TimeLineManager
+    public static class TimeLineManager
     {
         #region Fields
+        private static Dictionary<int, (float time, Action action)> timeLineEvents;
+        private static int nextID;
         #endregion
         #region Properties
+        public static Dictionary<int, (float time, Action action)> TimeLineEvents { get => timeLineEvents; set => timeLineEvents = value; }
         #endregion
         #region Constructors
+        static TimeLineManager()
+        {
+            TimeLineEvents = new Dictionary<int, (float time, Action action)>();
+            nextID = 0;
+
+        }
         #endregion
         #region Methods
+        /// <summary>
+        /// Adds an event to the timeline, that will be handled after a set amount of time.
+        /// </summary>
+        /// <param name="time">The time untill the event is executed/handled.</param>
+        /// <param name="action">The action that is ran once the time has passed.</param>
+        public static int AddEvent(float time, Action action)
+        {
+            TimeLineEvents.Add(nextID, (time, action));
+            return nextID++;
+        }
+
+        public static void Update(GameTime gameTime)
+        {
+            Dictionary<int, (float time, Action action)> tempTimeline = new Dictionary<int, (float time, Action action)>();
+            foreach (KeyValuePair<int, (float time, Action action)> timeLineEvent in timeLineEvents)
+            {
+                (float time, Action action) @event = timeLineEvent.Value;
+                @event.time -= gameTime.ElapsedGameTime.Milliseconds;
+                tempTimeline.Add(timeLineEvent.Key, @event);
+            }
+            TimeLineEvents = tempTimeline;
+        }
         #endregion
     }
 }
