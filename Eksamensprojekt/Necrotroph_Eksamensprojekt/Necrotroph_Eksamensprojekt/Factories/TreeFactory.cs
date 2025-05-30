f610a4f93f1535e52814f2b8c6b6e3113b2c9200
Hvemvilhvad <malthewbirch@gmail.com>
2025-05-30 13:45:16 +0200

    shader overlay works

--- Eksamensprojekt/Necrotroph_Eksamensprojekt/Necrotroph_Eksamensprojekt/Factories/TreeFactory.cs
+++ Eksamensprojekt/Necrotroph_Eksamensprojekt/Necrotroph_Eksamensprojekt/Factories/TreeFactory.cs
@@ -45,14 +45,14 @@ namespace Necrotroph_Eksamensprojekt.Factories
             switch (rnd.Next(1, 4))
             {
                 case 1: //big 1
-                    newTree.AddComponent<SpriteRenderer>(tree1, 1f, new Vector2(0.5f, 0.15f), new Vector2(0.55f, 0.85f));
-                    newTree.AddComponent<ShadowCaster>(100);
+                    newTree.AddComponent<SpriteRenderer>(tree1, 1f, new Vector2(0.5f, 0.15f), new Vector2(0.5f, 0.85f));
+                    newTree.AddComponent<ShadowCaster>(80);
                     animator.AddAnimation("Normal", tree1);
                     animator.AddAnimation("Seek", seeker1);
                     break;
                 case 2: //big 2
-                    newTree.AddComponent<SpriteRenderer>(tree2, 1f, new Vector2(0.5f, 0.15f), new Vector2(0.55f, 0.85f));
-                    newTree.AddComponent<ShadowCaster>(100);
+                    newTree.AddComponent<SpriteRenderer>(tree2, 1f, new Vector2(0.5f, 0.15f), new Vector2(0.5f, 0.85f));
+                    newTree.AddComponent<ShadowCaster>(80);
                     animator.AddAnimation("Normal", tree2);
                     animator.AddAnimation("Seek", seeker2);
                     break;
