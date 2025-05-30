8a46c5ec586e9b719f60f4ed1c5bad70de5c0977
Eleavate <echoovervad@gmail.com>
2025-05-30 12:59:39 +0200

    PLEASE

--- Eksamensprojekt/Necrotroph_Eksamensprojekt/Necrotroph_Eksamensprojekt/GameWorld.cs
+++ Eksamensprojekt/Necrotroph_Eksamensprojekt/Necrotroph_Eksamensprojekt/GameWorld.cs
@@ -76,34 +76,8 @@ namespace Necrotroph_Eksamensprojekt
 
 
             ScreenSize = new Vector2(_graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferHeight);
-            gameObjectsToAdd = new List<GameObject>();
-            gameObjectsToRemove = new List<GameObject>();
-            activeGameObjects = new List<GameObject>();
 
-            AddPlayer(Vector2.Zero);
-            //AddObject(TreePool.Instance.GetObject(new Vector2(200, 0)));
-            Map.GenerateMap();
-
-
-            InputHandler.AddHeldKeyCommand(Keys.D, new WalkCommand(Player.Instance, new Vector2(1, 0)));
-            InputHandler.AddHeldKeyCommand(Keys.A, new WalkCommand(Player.Instance, new Vector2(-1, 0)));
-            InputHandler.AddHeldKeyCommand(Keys.W, new WalkCommand(Player.Instance, new Vector2(0, -1)));
-            InputHandler.AddHeldKeyCommand(Keys.S, new WalkCommand(Player.Instance, new Vector2(0, 1)));
-
-            InputHandler.AddPressedKeyCommand(Keys.LeftShift, new SprintCommand(Player.Instance));
-            InputHandler.AddUnclickedCommand(Keys.LeftShift, new SprintCommand(Player.Instance));
-            
-            Graphics.PreferredBackBufferHeight = 1080;
-            Graphics.PreferredBackBufferWidth = 1920;
-            //Graphics.IsFullScreen = true;
-            Graphics.ApplyChanges();
-            ScreenSize = new Vector2(Graphics.PreferredBackBufferWidth, Graphics.PreferredBackBufferHeight);
-            
-            GameState.Initialize();
-
-            InputHandler.AddPressedKeyCommand(Keys.J, new SaveCommand());
-            InputHandler.AddPressedKeyCommand(Keys.L, new LoadCommand());
-
+           
             base.Initialize();
         }
 
@@ -116,16 +90,6 @@ namespace Necrotroph_Eksamensprojekt
             TreeFactory.LoadContent(Content);
             MemorabiliaFactory.LoadContent(Content);
             TextFactory.LoadContent(Content);
-
-            AddObject(EnemyFactory.CreateEnemy(new Vector2(-300, -300), EnemyType.Hunter));
-            AddObject(MemorabiliaFactory.CreateMemorabilia(new Vector2(500, 0)));
-            AddObject(MemorabiliaFactory.CreateMemorabilia(new Vector2(-500, 0)));
-            AddObject(MemorabiliaFactory.CreateMemorabilia(new Vector2(200, 0)));
-            AddObject(MemorabiliaFactory.CreateMemorabilia(new Vector2(-200, 0)));
-            AddObject(MemorabiliaFactory.CreateMemorabilia(new Vector2(600, 0)));
-
-
-            UIManager.AddUIObject(TextFactory.CreateTextObject(() => { return ItemsCollected + "/5"; }, Color.White, new Vector2 (50, 50), 1f));
 
             ShaderManager.SetSprite();
             
@@ -160,113 +124,9 @@ namespace Necrotroph_Eksamensprojekt
                 GameState = gameState;
                 GameState.Enter();
             }
-            gameObjectsToAdd.Clear();
-            foreach (GameObject gameObject in gameObjectsToRemove)
-            {
-                if (activeGameObjects.Contains(gameObject))
-                {
-                    activeGameObjects.Remove(gameObject);
-                }
-            }
-            gameObjectsToRemove.Clear();
         }
 
-        public void CheckCollision()
-        {
-            for (int i = 0; i < activeGameObjects.Count; i++)
-            {
-
-                for (int j = i + 1; j < activeGameObjects.Count; j++)
-                {
-                    if (activeGameObjects[i].CheckCollision(activeGameObjects[j]) && activeGameObjects[j].Active)
-                    {
-                        activeGameObjects[i].OnCollision(activeGameObjects[j]);
-                        activeGameObjects[j].OnCollision(activeGameObjects[i]);
-                    }
-                }
-
-            }
-
-        }
-
-        /// <summary>
-        /// Adds object to the gameworld during next update
-        /// </summary>
-        /// <param name="gameObject"></param>
-        public void AddObject(GameObject gameObject)
-        {
-            gameObject.Awake();
-            gameObjectsToAdd.Add(gameObject);
-        }
-        /// <summary>
-        /// Removes object from the gameworld during next update
-        /// </summary>
-        /// <param name="gameObject"></param>
-        public void RemoveObject(GameObject gameObject)
-        {
-            gameObjectsToRemove.Add(gameObject);
-        }
-        /// <summary>
-        /// Method to create player
-        /// </summary>
-        /// <param name="position"></param>
-        private void AddPlayer(Vector2 position)
-        {
-            Player newPlayer = Player.Instance;
-            newPlayer.AddComponent<Movable>();
-            newPlayer.AddComponent<SpriteRenderer>(Content.Load<Texture2D>("PlayerSprites/playerIdleSouthLightOn"), 1f, new Vector2(0.6f, 0.3f), new Vector2(0.5f, 0.85f));
-            newPlayer.AddComponent<Animator>();
-            newPlayer.AddComponent<LightEmitter>(0.2f);
-            //remember to add more animations
-            (newPlayer.GetComponent<Animator>()).AddAnimation("IdleLeftLightOff", Content.Load<Texture2D>("PlayerSprites/playerIdleWestLightOff"));
-            (newPlayer.GetComponent<Animator>()).AddAnimation("IdleLeftLightOn", Content.Load<Texture2D>("PlayerSprites/playerIdleWestLightOn"));
-            (newPlayer.GetComponent<Animator>()).AddAnimation("IdleDownLightOff", Content.Load<Texture2D>("PlayerSprites/playerIdleSouthLightOff"));
-            (newPlayer.GetComponent<Animator>()).AddAnimation("IdleDownLightOn", Content.Load<Texture2D>("PlayerSprites/playerIdleSouthLightOn"));
-            (newPlayer.GetComponent<Animator>()).PlayAnimation("IdleDownLightOn");
-            newPlayer.Transform.Scale = 0.25f;
-            AddObject(newPlayer);
-        }
-
-
-        public (List<LightEmitter> lightEmitters, List<ShadowInterval> shadowIntervals) GetShaderData()
-        {
-            List<LightEmitter> lightEmitters = new List<LightEmitter>();
-            List<ShadowInterval> shadowCasters = new List<ShadowInterval>();
-            foreach (GameObject gameObject in activeGameObjects)
-            {
-                Component shaderComponent;
-                if ((shaderComponent = gameObject.GetComponent<LightEmitter>()) is not null)
-                {
-                    lightEmitters.Add((LightEmitter)shaderComponent);
-                }
-                //else if ((shaderComponent = gameObject.GetComponent<ShadowCaster>()) is not null)
-                {
-
-                    //shadowCasters.Add((ShadowInterval)shaderComponent);
-                }
-            }
-            return (lightEmitters, shadowCasters);
-        }
-
-        public void CheckForWin()
-        {
-            if (ItemsCollected == 5 && !gameWon)
-            {
-                activeGameObjects.Clear();
-                UIManager.ActiveUIObjects.Clear();
-                gameWon = true;
-                string win = "You win";
-
-                UIManager.AddUIObject(TextFactory.CreateTextObject(() => { return win; }, Color.White, new Vector2(ScreenSize.X / 2, ScreenSize.Y / 2), 2f));
-            }
-        }
-
-        public void HearFromObserver(IObserver observer)
-        {
-            //currently appears under the shader :/
-            UIManager.AddUIObject(TextFactory.CreateTextObject(() => { return "Game Over"; }, Color.White, new Vector2(screenSize.X / 2, screenSize.Y / 2), 2f));
-        }
-        
+       
         #endregion
     }
 }
