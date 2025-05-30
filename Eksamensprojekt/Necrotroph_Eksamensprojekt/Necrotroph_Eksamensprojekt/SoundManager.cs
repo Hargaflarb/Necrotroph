12f9fce7e0e10a2c62647389e29ff85f6df279d5
Hargaflarb <emmabrocher@gmail.com>
2025-05-30 11:11:32 +0200

    started, don't merge, it doesn't work yet

---
+++ Eksamensprojekt/Necrotroph_Eksamensprojekt/Necrotroph_Eksamensprojekt/SoundManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Necrotroph_Eksamensprojekt
+{
+    public class SoundManager
+    {
+        #region Fields
+        private static SoundManager instance;
+        #endregion
+        #region Properties
+        public static SoundManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new SoundManager();
+                }
+                return instance;
+            }
+        }
+        #endregion
+        #region Constructors
+        #endregion
+        #region Methods
+        #endregion
+    }
+}
